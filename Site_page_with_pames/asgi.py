33bda5bb7519f40940e4398900b252bd07f6967b
Zimin0 <nikzim2004@gmail.com>
2020-08-25 13:45:56 +0300

    Starting project in django and adding new app 'games'

---
+++ Site_page_with_pames/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for Site_page_with_pames project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Site_page_with_pames.settings')
+
+application = get_asgi_application()
